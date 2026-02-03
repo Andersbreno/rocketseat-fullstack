f792466406c4437a06bd85009ee9a5761f684999
Anderson Breno <andersbreno@gmail.com>
2026-02-02 21:39:39 -0300

    Create index routes and use it

--- node/refound_api/src/app.ts
+++ node/refound_api/src/app.ts
@@ -1,6 +1,9 @@
 import express from "express"
+import "express-async-errors"
 import cors from "cors"
 
+
+import { routes } from "./routes"
 import { errorHandling } from "./middlewares/error-handling"
 import { AppError } from "./utils/AppError"
 import { z } from "zod"
@@ -9,7 +12,7 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-
+app.use(routes)
 app.use(errorHandling)
 
 export { app }
---
+++ node/refound_api/src/routes/index.ts
@@ -0,0 +1,17 @@
+import { Router } from "express";
+
+import { usersRoutes } from "./users-routes";
+import { sessionsRoutes } from "./session-routes";
+import { refundsRoutes } from "./refunds-routes";
+
+import { ensureAuthenticated } from "@/middlewares/ensure-authenticated";
+
+const routes = Router()
+
+routes.use("/users", usersRoutes)
+routes.use("/sessions", sessionsRoutes)
+
+routes.use(ensureAuthenticated)
+routes.use("/refunds", refundsRoutes)
+
+export { routes }
