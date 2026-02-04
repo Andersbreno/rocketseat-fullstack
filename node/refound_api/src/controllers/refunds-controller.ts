e5f962ee2f70f845e8fb47c2b0f1f9f697f99ebe
Anderson Breno <andersbreno@gmail.com>
2026-02-03 21:28:12 -0300

    Insert refund in db and list refounds

--- node/refound_api/src/controllers/refunds-controller.ts
+++ node/refound_api/src/controllers/refunds-controller.ts
@@ -1,10 +1,51 @@
+import { prisma } from "@/database/prisma";
 import { Request, Response } from "express";
+import { AppError } from "@/utils/AppError";
+import { z } from "zod"
 
+
+const CategoriesEnum = z.enum([
+    "food", 
+    "others", 
+    "services", 
+    "transport", 
+    "accommodation"
+])
 
 class RefundsController {
-    async create(request: Request, response: Response) {
-        response.json({ message: "ok" })
+  async create(request: Request, response: Response) {
+    const bodySchema = z.object({
+      name: z
+        .string()
+        .trim()
+        .min(1, { message: "Informe o nome da solicitação" }),
+      category: CategoriesEnum,
+      amount: z.number().positive({ message: "O valor precisa ser positivo" }),
+      filename: z.string().min(20),
+    });
+
+    const { name, category, amount, filename } = bodySchema.parse(request.body);
+
+    if (!request.user?.id) {
+      throw new AppError("Unauthorized", 401);
     }
+
+    const refund = await prisma.refunds.create({
+      data: {
+        name,
+        category,
+        amount,
+        filename,
+        userId: request.user.id,
+      },
+    });
+
+    response.status(201).json(refund);
+  }
+
+  async index(request: Request, response: Response) {
+    response.json({ message: "ok"})
+  }
 }
 
 export { RefundsController }
