a1c1643b1fb79c65a5cd62ef6d61e29c3c661f4f
Anderson Breno <andersbreno@gmail.com>
2026-02-07 16:15:41 -0300

    Create uploads files routes and .gitkeep
    Upload, delete and show files

--- node/refound_api/src/app.ts
+++ node/refound_api/src/app.ts
@@ -7,10 +7,13 @@ import { routes } from "./routes"
 import { errorHandling } from "./middlewares/error-handling"
 import { AppError } from "./utils/AppError"
 import { z } from "zod"
+import uploadConfig from "./configs/upload"
 
 const app = express()
 app.use(cors())
 app.use(express.json())
+
+app.use("/uploads", express.static(uploadConfig.UPLOADS_FOLDER))
 
 app.use(routes)
 app.use(errorHandling)
--- node/refound_api/src/routes/index.ts
+++ node/refound_api/src/routes/index.ts
@@ -3,6 +3,7 @@ import { Router } from "express";
 import { usersRoutes } from "./users-routes";
 import { sessionsRoutes } from "./session-routes";
 import { refundsRoutes } from "./refunds-routes";
+import { uploadsRoutes } from "./uploads-routes";
 
 import { ensureAuthenticated } from "@/middlewares/ensure-authenticated";
 
@@ -11,7 +12,10 @@ const routes = Router()
 routes.use("/users", usersRoutes)
 routes.use("/sessions", sessionsRoutes)
 
+
+
 routes.use(ensureAuthenticated)
 routes.use("/refunds", refundsRoutes)
+routes.use("/uploads", uploadsRoutes)
 
 export { routes }
